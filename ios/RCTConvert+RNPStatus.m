8ceec0748d7d29eca2a0efb4b7ac60e052b69441
Oleg Mikhailov <s2ferr@gmail.com>
2019-04-11 15:12:45 +0300

    only location and notif

--- ios/RCTConvert+RNPStatus.m
+++ ios/RCTConvert+RNPStatus.m
@@ -11,18 +11,9 @@
 @implementation RCTConvert (RNPStatus)
 
 RCT_ENUM_CONVERTER(RNPType, (@{ @"location" : @(RNPTypeLocation),
-                                @"camera" : @(RNPTypeCamera),
-                                @"microphone" : @(RNPTypeMicrophone),
-                                @"photo" : @(RNPTypePhoto),
-                                @"contacts" : @(RNPTypeContacts),
-                                @"event" : @(RNPTypeEvent),
-                                @"reminder" : @(RNPTypeReminder),
-                                @"bluetooth" : @(RNPTypeBluetooth),
+                                
                                 @"notification" : @(RNPTypeNotification),
-                                @"backgroundRefresh": @(RNPTypeBackgroundRefresh),
-                                @"speechRecognition": @(RNPTypeSpeechRecognition),
-                                @"mediaLibrary": @(RNPTypeMediaLibrary),
-                                @"motion": @(RNPTypeMotion)
+                                
                                 }),
                                 RNPTypeUnknown, integerValue)
 
