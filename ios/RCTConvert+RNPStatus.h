f325beb65e79790e2c3599a6ca5b27d66c04bbd2
Oleg Mikhailov <s2ferr@gmail.com>
2019-04-11 16:15:33 +0300

    remove media libs

--- ios/RCTConvert+RNPStatus.h
+++ ios/RCTConvert+RNPStatus.h
@@ -23,18 +23,7 @@ static NSString* RNPStatusRestricted = @"restricted";
 typedef NS_ENUM(NSInteger, RNPType) {
     RNPTypeUnknown,
     RNPTypeLocation,
-    RNPTypeCamera,
-    RNPTypeMicrophone,
-    RNPTypePhoto,
-    RNPTypeContacts,
-    RNPTypeEvent,
-    RNPTypeReminder,
-    RNPTypeBluetooth,
     RNPTypeNotification,
-    RNPTypeBackgroundRefresh,
-    RNPTypeSpeechRecognition,
-    RNPTypeMediaLibrary,
-    RNPTypeMotion
 };
 
 @interface RCTConvert (RNPStatus)
